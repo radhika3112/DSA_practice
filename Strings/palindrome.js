e901e9816ea8cd9806a6000a4f220b04d9658437
Radhika Parasana <89296586+radhika3112@users.noreply.github.com>
2023-12-13 15:01:33 +0530

    Update palindrome.js

--- Strings/palindrome.js
+++ Strings/palindrome.js
@@ -9,3 +9,23 @@ for(let i=0;i<str.length/2;i++)
 }
 
 console.log(`${str} is ${msg}`);
+
+
+
+
+
+let str="raar";
+let str2="";
+
+for(let i=str.length-1;i>=0;i--)
+{
+    str2+=str[i];
+}
+console.log(str2)
+if(str==str2)
+{
+    console.log("string is palindrome")
+}
+else{
+    console.log("string is not a palindrome")
+}
