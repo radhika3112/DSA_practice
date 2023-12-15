c36cb053631cf79e34c4249a52f0187b38663ea3
Radhika Parasana <89296586+radhika3112@users.noreply.github.com>
2023-12-15 10:16:39 +0530

    Create equivalentStrings.js

---
+++ Strings/equivalentStrings.js
@@ -0,0 +1,12 @@
+var arrayStringsAreEqual = function(word1, word2) {
+    let y=word1.join("");
+    let z=word2.join("");
+    if(y==z)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
